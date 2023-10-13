41f4b8afc3231f7f0b0a114d41fc33263781ff7f
CHUrururu <147632493+CHUrururu@users.noreply.github.com>
2023-10-13 20:50:06 +0900

    Add files via upload

---
+++ Greedy/Solution/2_큰 수의 법칙.py
@@ -0,0 +1,46 @@
+import sys
+input = sys.stdin.readline
+
+N, M, K = map(int, input().split())
+data = sorted(list(map(int, input().split())))
+
+first_max = data[-1]
+second_max = data[-2]
+
+count = (M // (K + 1)) * K
+count += M % (K + 1)
+
+result = count * first_max
+result += (M - count) * second_max
+
+print(result)
+
+
+"""
+    # Another Solution #
+    
+    import sys
+    input = sys.stdin.readline
+
+    N, M, K = map(int, input().split())
+    data = sorted(list(map(int, input().split())))
+
+    first_max = data[-1]
+    second_max = data[-2]
+    result = 0
+
+    while (True):
+        for _ in range(K):
+            if (M == 0):
+                break        
+            result += first_max
+            M -= 1
+
+        if (M == 0):
+            break
+    
+        result += second_max
+        M -= 1
+    
+    print(result)
+"""
