bc5502241b0b9779b21f5366b5c127731c86525b
CHUrururu <147632493+CHUrururu@users.noreply.github.com>
2023-10-15 02:08:10 +0900

    Add files via upload

---
+++ Greedy/Solution/7_문자열 뒤집기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+S = input().rstrip()
+group = 'none'
+count0 = 0
+count1 = 0
+
+for i in S:
+    if (i != group):
+        group = i
+        if (group == '0'):
+            count0 += 1
+        else:
+            count1 += 1
+            
+print(min(count0, count1))
