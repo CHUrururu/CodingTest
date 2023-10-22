7b0f3210c48599b2f082020d2ffea0818954192c
CHUrururu <147632493+CHUrururu@users.noreply.github.com>
2023-10-23 01:44:00 +0900

    Update 11_동전 개수의 최솟값 구하기.py

--- Greedy/Solution/11_동전 개수의 최솟값 구하기.py
+++ Greedy/Solution/11_동전 개수의 최솟값 구하기.py
@@ -19,24 +19,3 @@ for a in A:
             break
             
 print(count)
-
-
-
-"""
-    # Another Solution #
-    
-    N, K = map(int, input().split())
-    A = [0] * N
-
-    for i in range(N):
-        A[i] = int(input())
-
-    count = 0
-
-    for i in rangE(N - 1, -1, -1):
-    if A[i] <= K: # 현재 동전의 가치가 K보다 작거나 같으면 구성에 추가
-        count += int(K / A[i])
-        K = K % A[i] # K를 현재 동전을 사용하고 남은 금액으로 갱신
-
-    print(count)
-"""
