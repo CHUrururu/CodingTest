334aa0c537a6dd64b8919cee4eb1c7bf7768b4b7
CHUrururu <147632493+CHUrururu@users.noreply.github.com>
2023-10-23 01:47:55 +0900

    Update 15_최솟값을 만드는 괄호 배치 찾기.py

--- Greedy/Solution/15_최솟값을 만드는 괄호 배치 찾기.py
+++ Greedy/Solution/15_최솟값을 만드는 괄호 배치 찾기.py
@@ -17,28 +17,3 @@ for i in sum[1:]:
     answer -= i
     
 print(answer)
-
-
-
-"""
-    # Another Solution #
-    
-    answer = 0
-    A = list(map(str, input().split('-')))
-
-    def mySum(i): # -로 나뉜 그룹들의 합을 구하는 함수
-        sum = 0
-        temp = str(i).split('+')
-        for i in temp:
-            sum += int(i)
-        return sum
-
-    for i in range(len(A)):
-        temp = mySum(A[i])
-        if i == 0:
-            answer += temp # 가장 앞에 있는 값만 더하기
-        else:
-            answer -= temp # 뒷부분의 값은 합쳐서 빼기
-
-    print(answer)
-"""
