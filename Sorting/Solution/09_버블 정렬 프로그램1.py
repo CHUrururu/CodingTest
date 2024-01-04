d7e8f9679a362ae1acb0de217c97b3979bbe282e
CHUrururu <147632493+CHUrururu@users.noreply.github.com>
2024-01-05 02:02:33 +0900

    Update 09_버블 정렬 프로그램1.py

--- Sorting/Solution/09_버블 정렬 프로그램1.py
+++ Sorting/Solution/09_버블 정렬 프로그램1.py
@@ -11,7 +11,7 @@ numbers.sort()
 
 count = 0
 for i in range(N):
-    if (count < numbers[i][1] - i):
-        count = numbers[i][1] - i
+    if (count < numbers[i][1] - i): # 정렬 전 인덱스와 정렬 후 인덱스 비교
+        count = numbers[i][1] - i # 왼쪽으로 가장 많이 이동한 값
         
 print(count + 1)
