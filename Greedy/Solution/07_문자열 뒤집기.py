8435fd4de1600beb3b340402b198c84dbfb7ae79
CHUrururu <147632493+CHUrururu@users.noreply.github.com>
2024-02-15 19:42:57 +0900

    Update 07_문자열 뒤집기.py

--- Greedy/Solution/07_문자열 뒤집기.py
+++ Greedy/Solution/07_문자열 뒤집기.py
@@ -6,9 +6,9 @@ group = ''
 count0 = 0
 count1 = 0
 
-for i in S:
-    if (i != group):
-        group = i
+for num in S:
+    if (num != group):
+        group = num
         if (group == '0'):
             count0 += 1
         else:
