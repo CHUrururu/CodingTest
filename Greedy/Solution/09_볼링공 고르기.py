f49ac6cafdea3612817b103ea1e03615c371923a
CHUrururu <147632493+CHUrururu@users.noreply.github.com>
2024-02-16 15:18:11 +0900

    Update 09_볼링공 고르기.py

--- Greedy/Solution/09_볼링공 고르기.py
+++ Greedy/Solution/09_볼링공 고르기.py
@@ -11,3 +11,27 @@ for i in range(N - 1):
             count += 1
             
 print(count)
+
+
+
+"""
+    # Another Solution #
+
+    import sys
+    input = sys.stdin.readline
+
+    N, M = map(int, input().split())
+    data = list(map(int, input().split()))
+    weight = [0] * (M + 1)
+
+    for x in data:
+        weight[x] += 1
+
+    result = 0
+    for i in range(1, M + 1):
+        N -= weight[i]
+        result += weight[i] * N
+            
+
+    print(result)
+"""
