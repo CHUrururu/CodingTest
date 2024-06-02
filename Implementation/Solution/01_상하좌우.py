ac3779ca66a13efb44519f025c1dc33a8dd7d321
CHUrururu <147632493+CHUrururu@users.noreply.github.com>
2024-06-03 03:02:19 +0900

    Update 01_상하좌우.py

--- Implementation/Solution/01_상하좌우.py
+++ Implementation/Solution/01_상하좌우.py
@@ -1,3 +1,26 @@
+N = int(input())
+plans = list(input().split())
+x, y = 1, 1
+dirs = {'L' : [0, -1],
+        'R' : [0, 1],
+        'U' : [-1, 0],
+        'D' : [1, 0]}
+
+for plan in plans:
+    nx = x + dirs[plan][0]
+    ny = y + dirs[plan][1]
+    
+    if nx < 1 or ny < 1 or nx > N or ny > N:
+        continue
+    else:
+        x = nx
+        y = ny
+        
+print(x, y)
+
+
+
+"""
 import sys
 input = sys.stdin.readline
 
@@ -24,3 +47,5 @@ for plan in plans:
             X = temp
             
 print(X, Y)
+
+"""
